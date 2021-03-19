e02dbfe75524864b940da70abf515e2b09897e23
qa <qa@test.com>
2021-03-19 22:41:28 +0530

    add apis

--- CDN/js/index.js
+++ CDN/js/index.js
@@ -1,156 +1,102 @@
-window.addEventListener('DOMContentLoaded', function(event) {
-  console.log('DOM fully loaded and parsed');
-  websdkready();
-});
+(function(){
 
-function websdkready() {
-  var testTool = window.testTool;
-  if (testTool.isMobileDevice()) {
-    vConsole = new VConsole();
-  }
-  console.log("checkSystemRequirements");
-  console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+	console.log('checkSystemRequirements');
+	console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 
-  // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
-  // if (!china) ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.1/lib', '/av'); // CDN version default
-  // else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.9.1/lib', '/av'); // china cdn option
-  // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
-  ZoomMtg.preLoadWasm(); // pre download wasm file to save time.
-
-  var API_KEY = "YOUR_API_KEY";
-
-  /**
-   * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
-   * The below generateSignature should be done server side as not to expose your api secret in public
-   * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
-   */
-  var API_SECRET = "YOUR_API_SECRET";
-
-  // some help code, remember mn, pwd, lang to cookie, and autofill.
-  document.getElementById("display_name").value =
-    "CDN" +
-    ZoomMtg.getJSSDKVersion()[0] +
-    testTool.detectOS() +
-    "#" +
-    testTool.getBrowserInfo();
-  document.getElementById("meeting_number").value = testTool.getCookie(
-    "meeting_number"
-  );
-  document.getElementById("meeting_pwd").value = testTool.getCookie(
-    "meeting_pwd"
-  );
-  if (testTool.getCookie("meeting_lang"))
-    document.getElementById("meeting_lang").value = testTool.getCookie(
-      "meeting_lang"
-    );
-
-  document
-    .getElementById("meeting_lang")
-    .addEventListener("change", function (e) {
-      testTool.setCookie(
-        "meeting_lang",
-        document.getElementById("meeting_lang").value
-      );
-      testTool.setCookie(
-        "_zm_lang",
-        document.getElementById("meeting_lang").value
-      );
-    });
-  // copy zoom invite link to mn, autofill mn and pwd.
-  document
-    .getElementById("meeting_number")
-    .addEventListener("input", function (e) {
-      var tmpMn = e.target.value.replace(/([^0-9])+/i, "");
-      if (tmpMn.match(/([0-9]{9,11})/)) {
-        tmpMn = tmpMn.match(/([0-9]{9,11})/)[1];
-      }
-      var tmpPwd = e.target.value.match(/pwd=([\d,\w]+)/);
-      if (tmpPwd) {
-        document.getElementById("meeting_pwd").value = tmpPwd[1];
-        testTool.setCookie("meeting_pwd", tmpPwd[1]);
-      }
-      document.getElementById("meeting_number").value = tmpMn;
-      testTool.setCookie(
-        "meeting_number",
-        document.getElementById("meeting_number").value
-      );
-    });
-
-  document.getElementById("clear_all").addEventListener("click", function (e) {
-    testTool.deleteAllCookies();
-    document.getElementById("display_name").value = "";
-    document.getElementById("meeting_number").value = "";
-    document.getElementById("meeting_pwd").value = "";
-    document.getElementById("meeting_lang").value = "en-US";
-    document.getElementById("meeting_role").value = 0;
-    window.location.href = "/index.html";
-  });
-
-  // click join meeting button
-  document
-    .getElementById("join_meeting")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      var meetingConfig = testTool.getMeetingConfig();
-      if (!meetingConfig.mn || !meetingConfig.name) {
-        alert("Meeting number or username is empty");
-        return false;
-      }
-
-      
-      testTool.setCookie("meeting_number", meetingConfig.mn);
-      testTool.setCookie("meeting_pwd", meetingConfig.pwd);
-
-      var signature = ZoomMtg.generateSignature({
-        meetingNumber: meetingConfig.mn,
-        apiKey: API_KEY,
-        apiSecret: API_SECRET,
-        role: meetingConfig.role,
-        success: function (res) {
-          console.log(res.result);
-          meetingConfig.signature = res.result;
-          meetingConfig.apiKey = API_KEY;
-          var joinUrl = "/meeting.html?" + testTool.serialize(meetingConfig);
-          console.log(joinUrl);
-          window.open(joinUrl, "_blank");
-        },
-      });
-    });
-
-  function copyToClipboard(elementId) {
-    var aux = document.createElement("input");
-    aux.setAttribute("value", document.getElementById(elementId).getAttribute('link'));
-    document.body.appendChild(aux);  
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-  }
+    // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
+    // if (!china) ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.4/lib', '/av'); // CDN version default
+    // else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.7.4/lib', '/av'); // china cdn option 
+    // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
+    ZoomMtg.preLoadWasm();
+    ZoomMtg.prepareJssdk();
     
-  // click copy jon link button
-  window.copyJoinLink = function (element) {
-    var meetingConfig = testTool.getMeetingConfig();
-    if (!meetingConfig.mn || !meetingConfig.name) {
-      alert("Meeting number or username is empty");
-      return false;
-    }
-    var signature = ZoomMtg.generateSignature({
-      meetingNumber: meetingConfig.mn,
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
-      role: meetingConfig.role,
-      success: function (res) {
-        console.log(res.result);
-        meetingConfig.signature = res.result;
-        meetingConfig.apiKey = API_KEY;
-        var joinUrl =
-          testTool.getCurrentDomain() +
-          "/meeting.html?" +
-          testTool.serialize(meetingConfig);
-        document.getElementById('copy_link_value').setAttribute('link', joinUrl);
-        copyToClipboard('copy_link_value');
-        
-      },
-    });
-  };
+    testTool = window.testTool;
 
-}
+    $(document).ready(function(){
+        // e.preventDefault();
+        let params = (new URL(document.location));
+        var nonce = location.search.replace(/^.*?\=/, '');
+        // is the string "Jonathan"
+         if (nonce && nonce !== undefined && nonce !== null && nonce.trim() !== "") {
+             let requestObj = {
+                 nonce:nonce,
+             };
+             saveData = $.ajax({
+                 type: "POST",
+                 url: "https://www.whatsplan.app/rest/generateSignatureForZoom",
+                 data: requestObj,
+                 dataType: 'json',
+                 success: function(res){
+                    if(res.duration){
+                     setTimeout(()=>{
+                        window.location.reload();
+                     },res.duration);
+                    }
+                    if(res.statusCode==1)
+                            {
+                    ZoomMtg.init({
+                        leaveUrl: res.leaveurl,
+                        isSupportAV: true,
+                        success: function () {
+                            
+                            ZoomMtg.join(
+                                {
+                                    //meetingNumber: meetConfig.meetingNumber,
+                                    //userName: meetConfig.userName,
+                                    signature: res.signature,
+                                    apiKey: res.apiKey,
+                                    meetingNumber: res.meetingNumber,
+                                    userName: res.username,
+                                    userEmail: res.useremail,
+                                    passWord:res.password,
+                                    success: function(res){
+                                       // $('#nav-tool').hide();
+                                        console.log('join meeting success');
+                                    },
+                                    error: function(res) {
+                                        console.log(res);
+                                    }
+                                }
+                            );
+                    
+                        },
+                        error: function(res) {
+                            console.log(res);
+                        }
+                    });
+                }
+                else{
+                    console.log("meeting time exceeded");
+                }
+                }
+                 
+             });
+         } else {
+             console.log('err');
+             /*
+             $("body").html(
+                 '<div class="modal fade outerContainer" id="myModal" role="dialog">\
+                     <div class="innerContainer">\
+                         <div class="modal-dialog modal-md">\
+                           <div class="modal-content">\
+                             <div class="modal-header">\
+                               <button type="button" class="close" data-dismiss="modal">&times;</button>\
+                               <h4 class="modal-title">Your session expired</h4>\
+                             </div>\
+                             <div class="modal-body">\
+                               <p>Any confirmed transactions are saved, but you\'ll need to restart any searches or unfinished transactions.</p>\
+                             </div>\
+                             <div class="modal-footer">\
+                               <button type="button" class="btn btn-primary" data-dismiss="modal">Back to home</button>\
+                             </div>\
+                           </div>\
+                         </div>\
+                     </div>\
+                   </div>'
+             );
+             $("#myModal").modal();
+             */
+         }
+     });
+
+})();
